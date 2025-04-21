0453510befcd2ccd33e036cf7d07cef15a7fe309
brunovittoria <Bruno.Vittoria@umbrage.com>
2025-04-21 19:19:56 -0300

    docs: expand documentation for never type with detailed explanations and examples

--- modulo-02/30-aula-tipo-never/neverType.ts
+++ modulo-02/30-aula-tipo-never/neverType.ts
@@ -4,6 +4,16 @@
  * data: 11/11/2021
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#never
+ * 
+ * O tipo never é um tipo especial no TypeScript que representa valores que nunca ocorrem.
+ * Isso significa que uma função que retorna never nunca deve retornar um valor ou deve lançar uma exceção.
+ * 
+ * E usado principalmente em funções que lançam exceções ou que nunca retornam um valor.
+ * 
+ * Em projetos reais, o tipo never é útil para indicar que uma função não deve retornar um valor ou que um bloco de código nunca deve ser alcançado.
+ * 
+ * exemplo: uma função que lança uma exceção ou uma função que entra em um loop infinito.
+ *
  */
 
 // ==> Exemplo 01: Never - Throw Exception
